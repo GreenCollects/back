c5ff732f32f9a1421224543649fbdc3fbfb9571b
Quentin <quentin38590@gmail.com>
2022-02-23 16:29:34 +0100

    Add Ranking and Point API, Views and url

--- project/greencollect_api/urls.py
+++ project/greencollect_api/urls.py
@@ -6,12 +6,20 @@ from .views import (
     CommunityCollectView,
     PreventiveView,
     PreventiveDetailsView,
+    PointView,
+    PointDetailsView,
+    RatingView,
+    RatingDetailsView
 )
 
 urlpatterns = [
-    path('waste', WasteView.as_view()),
+    path('waste/', WasteView.as_view()),
     path('communityCollect/', CommunityCollectView.as_view()),
     path('communityCollectDetails/<int:id>/', CommunityCollectDetailsView.as_view()),
     path('preventive/', PreventiveView.as_view()),
     path('preventiveDetails/<int:id>/', PreventiveDetailsView.as_view()),
+    path('points/', PointView.as_view()),
+    path('pointDetails/<int:id>/', PointDetailsView.as_view()),
+    path('rating/', RatingView.as_view()),
+    path('rating/<int:id>/', RatingDetailsView.as_view())
 ]
